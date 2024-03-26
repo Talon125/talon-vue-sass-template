e6d814b9cd4f506652235cffb75c14e2debb8363
Oktalon-Szoradi <szoradi.o19@htlwienwest.at>
2024-03-26 22:51:38 +0100

    Use slot (child) instead of content attribute

--- src/views/ComponentsView.vue
+++ src/views/ComponentsView.vue
@@ -14,7 +14,7 @@ const refreshPage = () => {
     <h2>Banners:</h2>
     <Banner topic="Default Banner">
       Yeah. You can also put things in here!<br />
-      <Button content="Refresh" @click="refreshPage" />
+      <Button @click="refreshPage">Refresh</Button>
     </Banner>
     <Banner
       v-for="color in colors"
@@ -28,37 +28,32 @@ const refreshPage = () => {
       <tbody>
         <tr>
           <th>Normal button:</th>
-          <td><Button content="Button" /></td>
+          <td><Button>Button</Button></td>
         </tr>
         <tr>
           <th>Default button:</th>
-          <td><Button content="Button" defaultButton /></td>
+          <td><Button defaultButton>Button</Button></td>
         </tr>
         <tr v-for="color in colors" :key="color">
           <th>{{ color }} buttons:</th>
-          <td><Button content="Button" :color="color.toLowerCase()" /></td>
-          <td><Button content="Button" :color="color.toLowerCase()" defaultButton /></td>
-          <td><Button content="Button" :color="color.toLowerCase()" disabled /></td>
-          <td><Button content="Button" :color="color.toLowerCase()" href="#" /></td>
+          <td><Button :color="color.toLowerCase()">Button</Button></td>
+          <td><Button :color="color.toLowerCase()" defaultButton>Button</Button></td>
+          <td><Button :color="color.toLowerCase()" disabled>Button</Button></td>
+          <td><Button :color="color.toLowerCase()" href="#">Button</Button></td>
           <td>
-            <Button
-              content="Button"
-              :color="color.toLowerCase()"
-              href="#"
-              defaultButton
-            />
+            <Button :color="color.toLowerCase()" href="#" defaultButton>Button</Button>
           </td>
         </tr>
         <tr>
           <th>Disabled button:</th>
-          <td><Button content="Button" disabled /></td>
+          <td><Button disabled>Button</Button></td>
         </tr>
         <tr>
           <th>Link button:</th>
-          <td><Button content="Button" href="#" /></td>
-          <td><Button content="Button" href="#" defaultButton /></td>
-          <td><Button content="Button" href="#" stayHere /></td>
-          <td><Button content="Button" href="#" defaultButton stayHere /></td>
+          <td><Button href="#">Button</Button></td>
+          <td><Button href="#" defaultButton>Button</Button></td>
+          <td><Button href="#" stayHere>Button</Button></td>
+          <td><Button href="#" defaultButton stayHere>Button</Button></td>
         </tr>
       </tbody>
     </table>
