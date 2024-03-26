dd0b73c9b02c92fbe72e15085ed6d16cac870f8f
Oktalon-Szoradi <szoradi.o19@htlwienwest.at>
2024-03-26 23:16:26 +0100

    Move external link icon into its own component

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -12,18 +12,7 @@
     <span>
       <slot></slot>
     </span>
-    <svg
-      v-if="!stayHere"
-      xmlns="http://www.w3.org/2000/svg"
-      width="12"
-      height="12"
-      viewBox="0 0 12 12"
-    >
-      <path
-        :fill="color ? 'white' : 'black'"
-        d="M6 1h5v5L8.86 3.85 4.7 8 4 7.3l4.15-4.16zM2 3h2v1H2v6h6V8h1v2a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1"
-      />
-    </svg>
+    <IconExternalLink v-if="!stayHere" :fillColor="color ? 'white' : 'black'" />
   </a>
   <button
     v-else
@@ -38,6 +27,8 @@
 </template>
 
 <script setup>
+import IconExternalLink from '../components/icons/IconExternalLink.vue'
+
 const props = defineProps({
   defaultButton: { type: Boolean },
   color: { type: String },
