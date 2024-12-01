df7d454be65534a49b42f83453085a963178b7ef
Talon125 <talonszoradi@gmail.com>
2024-12-01 03:14:03 +0100

    Add container definitions

--- src/App.vue
+++ src/App.vue
@@ -20,4 +20,30 @@ html {
   background-size: cover;
   height: 100%;
 }
+
+.container {
+  margin-right: auto;
+  margin-left: auto;
+  width: 100%;
+
+  @media (width >= 576px) {
+    max-width: 540px;
+  }
+
+  @media (width >= 768px) {
+    max-width: 720px;
+  }
+
+  @media (width >= 992px) {
+    max-width: 960px;
+  }
+
+  @media (width >= 1200px) {
+    max-width: 1140px;
+  }
+
+  @media (width >= 1400px) {
+    max-width: 1320px;
+  }
+}
 </style>
