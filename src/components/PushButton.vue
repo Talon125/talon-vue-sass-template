24f0bd2000da21b70a126286b552c307f4b61055
Talon125 <talonszoradi@gmail.com>
2025-06-15 00:42:51 +0200

    Finish buttons

--- src/components/PushButton.vue
+++ src/components/PushButton.vue
@@ -3,16 +3,20 @@
     v-if="href"
     :href="href"
     :target="stayHere ? '_self' : '_blank'"
-    :disabled="disabled"
     :class="`
+      ${disabled ? 'disabled' : ''}
       ${defaultButton ? 'default' : ''}
       ${color ? color : ''}
     `"
   >
-    <span>
-      <slot></slot>
+    <span class="bottom"></span>
+    <span class="side"></span>
+    <span class="face">
+      <span>
+        <slot></slot>
+        <IconExternalLink v-if="!stayHere" fillColor="white" />
+      </span>
     </span>
-    <IconExternalLink v-if="!stayHere" :fillColor="color ? 'white' : 'black'" />
   </a>
   <button
     v-else
@@ -22,7 +26,11 @@
       ${color ? color : ''}
     `"
   >
-    <slot></slot>
+    <span class="bottom"></span>
+    <span class="side"></span>
+    <span class="face">
+      <slot></slot>
+    </span>
   </button>
 </template>
 
@@ -42,308 +50,127 @@ defineProps({
 @use 'sass:color';
 @use '../assets/variables.scss' as vars;
 
-$button-inner-shadow: inset 0 -1px 4px hsl(0deg 0% 0% / 25%);
-$button-inner-sheen-weak: inset 0 1.5px 3px 1px hsl(0deg 0% 100% / 50%);
-$button-inner-sheen: inset 0 1.5px 1px 1px hsl(0deg 0% 100%);
+$corner-roundness: 8px;
+$transdur-mouseenter: 250ms;
+$transdur-mouseleave: 1000ms;
+$transdur-press: 50ms;
 
-@mixin button-colored($hue) {
-  /* $color: hsl($hue, 100%, 50%);
-  $luminance: (0.299 * color.red($color) + 0.587 * color.green($color) + 0.114 * color.blue($color)) / 255;
-  $text-shadow-color: if($luminance < 0.5, 0%, 100%);
-  $text-color: if($luminance < 0.5, 100%, 0%); */
-
-  $text-shadow-color: 0%;
-  $text-color: 100%;
-
-  box-shadow:
-    0 2px 0 hsl($hue, 75%, 25%),
-    $button-inner-sheen-weak,
-    $button-inner-shadow,
-    vars.$outer-shadow;
-  background:
-    vars.$reflection-moderate,
-    linear-gradient(to bottom, hsl($hue, 75%, 85%), hsl($hue, 75%, 35%), hsl($hue, 75%, 50%));
-  text-shadow: 0 1px 3px hsla(0deg, 0%, $text-shadow-color, 75%);
-  color: hsla(0deg, 0%, $text-color, 85%);
-  font-size: 100%;
-
-  /* Hover Color */
-  &::before {
-    background:
-      vars.$reflection-major,
-      linear-gradient(to bottom, hsl($hue, 100%, 85%), hsl($hue, 100%, 40%), hsl($hue, 100%, 65%));
-  }
-
-  /* Active Color */
-  &::after {
-    background:
-      vars.$reflection-moderate,
-      linear-gradient(to bottom, hsl($hue, 90%, 75%), hsl($hue, 90%, 25%), hsl($hue, 90%, 40%));
-  }
-
-  /* Disabled Style */
-  &:disabled {
-    box-shadow:
-      0 2px 0 hsl($hue, 100%, 33%, 25%),
-      inset 0 1.5px 2px 1px hsl(0deg 0% 100% / 15%),
-      inset 0 -1px 4px hsl(0deg 0% 0% / 5%),
-      0 2px 4px hsl(0deg 0% 50% / 10%);
-    background:
-      linear-gradient(
-        to bottom,
-        hsl(0deg 0% 100% / 5%),
-        hsl(0deg 0% 100% / 10%) 50%,
-        transparent 50%
-      ),
-      linear-gradient(to bottom, hsl($hue, 100%, 50%, 25%), hsl($hue, 100%, 40%, 25%));
-    text-shadow: 0 1px 3px hsla(0deg, 0%, $text-shadow-color, 17.5%);
-    color: hsla(0deg, 0%, $text-color, if($text-color == 0%, 17.5%, 60%));
-  }
-
-  &:not(:disabled) {
-    /* Active */
-    &::after,
-    &:active {
-      box-shadow:
-        inset 0 2px 8px hsla($hue, 100%, 17.5%, 75%),
-        0 1px 2px hsl(0deg 0% 100% / 50%);
-      text-shadow: none;
-      color: hsla(0deg, 0%, $text-color, 50%);
-    }
-
-    /* Hover */
-    &:not(:active)::before,
-    &:hover:not(:active) {
-      box-shadow:
-        0 2px 0 hsl($hue, 75%, 40%),
-        $button-inner-sheen-weak,
-        $button-inner-shadow,
-        0 2px 8px 4px hsla($hue, 80%, 80%, 100%);
-      text-shadow: 0 1px 4px hsla(0deg, 0%, $text-shadow-color, 90%);
-      color: hsla(0deg, 0%, $text-color, 100%);
-    }
-  }
-
-  /* Default Pulse */
-  &.default:not(:disabled, :hover, :active) {
-    box-shadow:
-      0 2px 0 hsl($hue, 50%, 40%),
-      inset 0 0 4px 2px hsla($hue, 100%, 60%, 85%),
-      $button-inner-sheen-weak,
-      $button-inner-shadow,
-      vars.$outer-shadow;
-    background:
-      vars.$reflection-weak,
-      linear-gradient(to bottom, hsl($hue, 80%, 95%), hsl($hue, 80%, 45%), hsl($hue, 80%, 60%));
-
-    &::before {
-      box-shadow:
-        0 2px 0 hsl($hue, 100%, 40%),
-        inset 0 0 8px 4px hsla($hue, 100%, 60%, 85%),
-        $button-inner-sheen-weak,
-        $button-inner-shadow,
-        0 2px 8px 4px hsla($hue, 80%, 50%, 75%);
-      background:
-        vars.$reflection-weak,
-        linear-gradient(to bottom, hsl($hue, 75%, 85%), hsl($hue, 75%, 35%), hsl($hue, 75%, 50%));
-    }
-
-    /* animation: pulse-anim 1s linear infinite alternate; */
-  }
+.bottom,
+.side,
+.face {
+  padding: 0.5em 2em;
 }
 
-/* ---------------------------------------------------------------------------------------------------- */
-
-a,
-button {
+.bottom,
+.side {
   box-sizing: border-box;
-  display: inline-block;
-  position: relative;
-  transition-property: box-shadow, color, opacity, text-shadow, transform;
-  z-index: 0;
-  margin: 1em;
+  position: absolute;
+  top: -4px;
+  left: -1px;
+  transform: translateY(4px);
+  transition-duration: $transdur-mouseleave;
   border: none;
-  border-radius: 8px;
-  box-shadow:
-    0 2px 0 hsl(0deg 0% 50%),
-    $button-inner-sheen,
-    $button-inner-shadow,
-    vars.$outer-shadow;
-  background:
-    vars.$reflection-strong, linear-gradient(to bottom, hsl(0deg 0% 95%), hsl(0deg 0% 80%));
-  cursor: pointer;
-  padding: 8px 24px;
-  text-decoration: none;
-  text-shadow: 0 1px 0 white;
-  line-height: 1em;
-  white-space: nowrap;
-  color: hsl(0deg 0% 13%);
-  font-size: 100%;
-  user-select: none;
-
-  /* Hover Color */
-  &::before {
-    position: absolute;
-    top: 0;
-    left: 0;
-    transition-duration: 0.1s;
-    opacity: 0;
-    z-index: -1;
-    margin: 0;
-    border-radius: 8px;
-    background:
-      vars.$reflection-strong, linear-gradient(to bottom, hsl(200deg 90% 95%), hsl(200deg 80% 80%));
-    padding: 0;
-    width: 100%;
-    height: 100%;
-    content: '';
-  }
-
-  /* Active Color */
-  &::after {
-    position: absolute;
-    top: 0;
-    left: 0;
-    opacity: 0;
-    z-index: -1;
-    margin: 0;
-    border-radius: 8px;
-    background:
-      vars.$reflection-major, linear-gradient(to bottom, hsl(200deg 80% 95%), hsl(200deg 60% 60%));
-    padding: 0;
-    width: 100%;
-    height: 100%;
-    content: '';
-  }
-
-  &:not(:hover)::before {
-    transition-duration: 1s;
-    opacity: 0;
-  }
-
-  /* Disabled Style */
-  &:disabled {
-    box-shadow:
-      0 2px 0 hsl(0deg 0% 80% / 25%),
-      inset 0 1.5px 1px 1px hsl(0deg 0% 100% / 25%),
-      inset 0 -1px 4px hsl(0deg 0% 0% / 5%),
-      0 2px 4px hsl(0deg 0% 50% / 10%);
-    background:
-      vars.$reflection-weak,
-      linear-gradient(to bottom, hsl(0deg 0% 95% / 50%), hsl(0deg 0% 94% / 50%));
-    cursor: not-allowed;
-    color: hsl(0deg 0% 26% / 33%);
-    backdrop-filter: blur(8px);
-  }
-
-  &:not(:disabled) {
-    /* Active */
-    &::after,
-    &:active {
-      transform: translateY(2px);
-      transition-duration: 0.1s;
-      box-shadow:
-        inset 0 2px 8px hsl(200deg 100% 17.5% / 75%),
-        0 1px 2px hsl(0deg 0% 100% / 50%);
-      text-shadow: none;
-      color: hsl(0deg 0% 13% / 50%);
-    }
-
-    /* Hover */
-    &:not(:active)::before,
-    &:hover:not(:active) {
-      box-shadow:
-        0 2px 0 hsl(200deg 50% 50%),
-        $button-inner-sheen,
-        $button-inner-shadow,
-        0 2px 8px 4px hsl(200deg 80% 80% / 100%);
-      text-shadow: 0 1px 1px white;
-    }
-
-    &:hover:not(:active)::before {
-      opacity: 1;
-    }
-
-    &:active::after {
-      transform: translateY(0);
-      opacity: 1;
-    }
-  }
-
-  /* Default Pulse */
-  &.default:not(:disabled, :hover, :active) {
-    box-shadow:
-      0 2px 0 hsl(190deg 50% 40%),
-      inset 0 0 2px 1px hsl(190deg 100% 60% / 85%),
-      $button-inner-sheen,
-      $button-inner-shadow,
-      vars.$outer-shadow;
-    background:
-      vars.$reflection-strong, linear-gradient(to bottom, hsl(200deg 50% 95%), hsl(200deg 40% 80%));
-
-    &::before {
-      position: absolute;
-      top: 0;
-      left: 0;
-      transition-duration: 0.1s;
-      opacity: 0;
-      margin: 0;
-      border-radius: 8px;
-      box-shadow:
-        0 2px 0 hsl(190deg 100% 40%),
-        inset 0 0 4px 2px hsl(190deg 100% 60% / 85%),
-        $button-inner-sheen,
-        $button-inner-shadow,
-        0 2px 8px 4px hsl(190deg 80% 50% / 75%);
-      background:
-        vars.$reflection-strong,
-        linear-gradient(to bottom, hsl(200deg 90% 95%), hsl(200deg 80% 80%));
-      padding: 0;
-      width: 100%;
-      height: 100%;
-      animation: fade 1s linear infinite alternate;
-      content: '';
-    }
-
-    /* animation: pulse-anim 1s linear infinite alternate; */
-  }
+  border-radius: #{$corner-roundness + 1px};
+  box-shadow: 0 1px hsla(0deg 0% 100% / 25%);
+  background: linear-gradient(to bottom, transparent, hsl(0deg 0% 15% / 50%));
+  width: calc(100% + 2px);
+  height: calc(100% + 4px);
 }
 
-a span {
+.bottom {
+  opacity: 0;
+  background: linear-gradient(to bottom, transparent, hsl(0deg 0% 100% / 50%));
+}
+
+.face {
+  box-sizing: border-box;
+  display: block;
+  position: relative;
+  transition-duration: $transdur-mouseleave;
+  border-radius: $corner-roundness;
+  box-shadow:
+    inset 0 0 0 1px hsl(0deg 0% 100% / 25%),
+    0 0 0 1px hsla(0deg 0% 0% / 75%);
+  background:
+    vars.$reflection-soft, vars.$reflection-weak,
+    radial-gradient(ellipse 100% 33% at bottom, hsl(0deg 0% 100% / 10%), transparent),
+    radial-gradient(ellipse at bottom left, transparent 75%, hsl(0deg 0% 100% / 15%)),
+    radial-gradient(ellipse at bottom right, transparent 75%, hsl(0deg 0% 100% / 15%)),
+    linear-gradient(to bottom, hsl(0deg 0% 0% / 10%), hsl(0deg 0% 0% / 30%), hsl(0deg 0% 0% / 20%));
+  width: 100%;
+  height: 100%;
+  text-shadow: 0 2px 2px hsl(0deg 0% 0% / 25%);
+  color: hsla(0deg 0% 100% / 90%);
+  font-size: 1rem;
+}
+
+a .face {
   text-decoration: underline;
 }
 
-.red {
-  @include button-colored(vars.$red);
-}
+a,
+button {
+  display: inline-block;
+  position: relative;
+  transition-duration: 250ms;
+  margin: 0.5em 0.5em 0;
+  border: none;
+  background: none;
+  cursor: pointer;
+  padding: 0;
+  text-decoration: none;
+  will-change: transform;
 
-.orange {
-  @include button-colored(vars.$orange);
-}
-
-.yellow {
-  @include button-colored(vars.$yellow);
-}
-
-.green {
-  @include button-colored(vars.$green);
-}
-
-.blue {
-  @include button-colored(vars.$blue);
-}
-
-.purple {
-  @include button-colored(vars.$purple);
-}
-
-@keyframes fade {
-  0% {
-    opacity: 0;
+  &::after {
+    content: none;
   }
 
-  100% {
-    opacity: 1;
+  &:disabled {
+    opacity: 0.5;
+    cursor: not-allowed;
+
+    .face {
+      color: hsla(0deg 0% 100% / 50%);
+    }
+  }
+
+  &:not(:disabled):hover {
+    .bottom {
+      transition-duration: $transdur-mouseenter;
+      opacity: 1;
+      box-shadow: 0 0 8px 4px hsla(0deg 0% 100% / 50%);
+    }
+
+    .face {
+      transition-duration: $transdur-mouseenter;
+      filter: brightness(1.5);
+    }
+  }
+
+  /* &:not(:disabled):focus, */
+  &:not(:disabled):active {
+    .bottom {
+      transition-duration: $transdur-press;
+      opacity: 0;
+      box-shadow: none;
+    }
+
+    .face {
+      transform: translateY(3px);
+      transition-duration: $transdur-press;
+      box-shadow:
+        inset 0 0 0 1px hsl(0deg 0% 100% / 25%),
+        0 0 0 1px hsla(0deg 0% 0% / 75%),
+        inset 0 0 12px hsl(0deg 0% 0% / 75%);
+      color: hsla(0deg 0% 100% / 50%);
+      filter: brightness(0.75);
+
+      span {
+        opacity: 0.5;
+        color: hsla(0deg 0% 100% / 90%);
+        /* filter: brightness(0.75); */
+      }
+    }
   }
 }
 </style>
