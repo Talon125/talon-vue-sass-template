40bfabe38528a23f3c28d3b723e0292e4356050c
Talon125 <talonszoradi@gmail.com>
2025-06-14 11:28:47 +0200

    Update PageNotFound.vue to use GlassCard

--- src/views/PageNotFound.vue
+++ src/views/PageNotFound.vue
@@ -1,12 +1,17 @@
+<script setup lang="ts">
+import GlassCard from '@/components/GlassCard.vue'
+</script>
+
 <template>
   <main class="container">
     <div class="mrow">
-      <h1>404</h1>
-      <span>
-        There's no <code>{{ $route.fullPath }}</code
-        >!!!11!1!1
-      </span>
-      Grrr...&nbsp;&nbsp;&gt;&omega;&lt;
+      <GlassCard title="404" class="text-center miau">
+        <p>
+          There's no <code>{{ $route.fullPath }}</code
+          >!!!11!1!1
+        </p>
+        Grrr...&nbsp;&nbsp;&gt;&omega;&lt;
+      </GlassCard>
     </div>
   </main>
 </template>
@@ -18,5 +23,9 @@
   align-items: center;
   justify-content: center;
   height: 50vh;
+}
+
+.miau {
+  min-width: 50vw;
 }
 </style>
