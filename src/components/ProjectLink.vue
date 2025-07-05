7771c0212e21bbd0138268926b55a7b195105aa9
Talon125 <talonszoradi@gmail.com>
2025-07-05 20:13:54 +0200

    Make ProjectLink component

---
+++ src/components/ProjectLink.vue
@@ -0,0 +1,41 @@
+<template>
+  <GlassCard class="root" :clear="true">
+    <ImageHandler :src="icon_src ?? ''" :alt="icon_alt" />
+    <h1>
+      {{ title }}
+    </h1>
+    <slot />
+  </GlassCard>
+</template>
+
+<script setup lang="ts">
+import GlassCard from '@/components/GlassCard.vue'
+import ImageHandler from '@/components/ImageHandler.vue'
+
+defineProps({
+  title: { type: String },
+  icon_src: { type: String },
+  icon_alt: { type: String }
+})
+</script>
+
+<style lang="scss" scoped>
+@use 'sass:color';
+@use '../assets/variables.scss' as vars;
+
+.root {
+  margin-top: 0;
+}
+
+:deep(img) {
+  border-radius: 4px;
+  box-shadow: 0 0 0 1px hsl(0deg 0% 0% / 50%), 0 0 0 2px hsl(0deg 0% 100% / 50%);
+  width: 100%;
+}
+
+h1 {
+  margin: 0.25em 0;
+  color: hsla(0deg 0% 100% / 75%);
+  font-weight: lighter;
+}
+</style>
