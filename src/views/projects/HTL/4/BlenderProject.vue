02eb000e6bb6079476305b7b60990b146009e2db
Talon125 <talonszoradi@gmail.com>
2025-07-06 03:39:33 +0200

    Add in some info

--- src/views/projects/HTL/4/BlenderProject.vue
+++ src/views/projects/HTL/4/BlenderProject.vue
@@ -1,26 +1,58 @@
 <script setup lang="ts">
 import NavigationButtons from '@/components/NavigationButtons.vue'
-// import PushButton from '@/components/PushButton.vue'
+import PushButton from '@/components/PushButton.vue'
 import GlassCard from '@/components/GlassCard.vue'
+
+const PHOTO_NAMES_19 = [
+  'SuzanneCloseupDonut',
+  'SuzanneCloseupPlanet',
+  'SuzanneDonutKiwi',
+  'SuzannePlanets'
+]
+
+const PHOTO_NAMES_20 = ['Overview1', 'Overview2', 'SuzanneDonutChocolate']
+
+function breakIntoWords(s: string): string {
+  const words = replaceGermanChars(s).split(/(?=[A-Z])/)
+  return words.join(' ')
+}
+
+function getCorrectPath(index: number): string {
+  if (index < PHOTO_NAMES_19.length) {
+    return `/img/blender/Render_20230619-${PHOTO_NAMES_19[index]}.png`
+  } else {
+    return `/img/blender/Render_20230620-${
+      PHOTO_NAMES_20[index - PHOTO_NAMES_19.length]
+    }.png`
+  }
+}
 </script>
 
 <template>
   <main class="container">
-    <NavigationButtons />
-    <GlassCard title="Blender"> Coming soon... </GlassCard>
+    <NavigationButtons :queryIndex="1" :queryParams="{ scrollTo: 'HTL-4' }" />
+    <GlassCard title="3D (Blender)" :noCenter="true">
+      <b>Teacher's grade: 1-2 </b>
+      <p>
+        Ah, the 3D modeling software. The theme was space, but at the time I
+        only had a donut, so I improvised:
+      </p>
+      <p>
+        Suzanne is taking over the universe, I guess and it seems she's turning
+        planets into donuts.
+      </p>
+      <div class="text-center">
+        <PushButton
+          color="purple"
+          href="/other/BlenderProject.zip"
+          :stayHere="true"
+        >
+          Download<code>&emsp;BlenderProject.zip</code>
+        </PushButton>
+      </div>
+    </GlassCard>
+    <GlassCard> Images coming soon... </GlassCard>
   </main>
 </template>
 
-<style scoped lang="scss">
-.mrow {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  height: 50vh;
-}
-
-.miau {
-  min-width: 50vw;
-}
-</style>
+<style scoped lang="scss"></style>
--- src/views/projects/HTL/4/PhotoProject.vue
+++ src/views/projects/HTL/4/PhotoProject.vue
@@ -2,27 +2,64 @@
 import NavigationButtons from '@/components/NavigationButtons.vue'
 // import PushButton from '@/components/PushButton.vue'
 import GlassCard from '@/components/GlassCard.vue'
+
+const PHOTO_NAMES = [
+  'Becken',
+  'BeckenLinks',
+  'BeckenRechts',
+  'Ente',
+  'Enten',
+  'FontaeneBaeume',
+  'FontaeneBaeumeDunkel',
+  'FontaeneBaeumeLang',
+  'FontaeneBaeumeLinsenreflexion',
+  'FontaeneGras',
+  'FontaenePlatscherBlume',
+  'FontaenePlatscherEnte',
+  'FontaenePlatscherLang',
+  'FontaenePlatscherLangEnte',
+  'FontaenePlatscherZiegel',
+  'FontaenenEingang',
+  'FontaenenUebersicht',
+  'Papagenobrunnen',
+  'PapagenobrunnenLang',
+  'PapagenobrunnenMuschelschaleChaos',
+  'PapagenobrunnenMuschelschaleFluss',
+  'PapagenobrunnenMuschelschaleKurve',
+  'SteinFlussKurz',
+  'SteinFlussLang',
+  'WasserEingefrohren',
+  'WasserEingefrohrenKlar',
+  'WasserEingefrohrenKuehl',
+  'WasserEingefrohrenNiedrig',
+  'WasserEingefrohrenWiese',
+  'WasserNebel'
+]
+
+function replaceGermanChars(s: string): string {
+  return s.replace(/ae/gi, 'ä').replace(/oe/gi, 'ö').replace(/ue/gi, 'ü')
+}
+
+function breakIntoWords(s: string): string {
+  const words = replaceGermanChars(s).split(/(?=[A-Z])/)
+  return words.join(' ')
+}
 </script>
 
 <template>
   <main class="container">
-    <NavigationButtons />
-    <GlassCard title="Photo"> Coming soon...
-    <!-- {{ $route }} -->
-     </GlassCard>
+    <NavigationButtons :queryIndex="1" :queryParams="{ scrollTo: 'HTL-4' }" />
+    <GlassCard title="Kurpark Oberlaa" :noCenter="true">
+      <b>Teacher's grade: 1.2 </b>
+      <p>
+        I actually took a total of 108 photos! But here are the best 30 (edited,
+        not raw) With a borrowed <b>Nikon D300S</b>, I was at this park for
+        around 4 hours.
+      </p>
+      <b>By the way:</b> The names of the photos are in German.
+    </GlassCard>
+    <GlassCard> Images coming soon... </GlassCard>
   </main>
 </template>
 
-<style scoped lang="scss">
-.mrow {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  height: 50vh;
-}
-
-.miau {
-  min-width: 50vw;
-}
-</style>
+<style scoped lang="scss"></style>
